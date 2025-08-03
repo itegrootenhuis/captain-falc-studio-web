31e7949fbf32139894bda919f047f66381591114
Ian TeGrootenhuis <ian.tegrootenhuis@meetrise.com>
2025-08-03 19:58:31 -0400

    unbolding subtitle contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -154,7 +154,7 @@ export default async function IndexPage() {
           <h2 className="text-5xl font-bold mb-6 text-center">
             {contactPage.contactFromTitle}
           </h2>
-          <p className="text-xl font-bold mb-6 text-center">
+          <p className="text-xl mb-6 text-center">
             {contactPage.contactFromSubTitle}
           </p>
           <ContactForm />
